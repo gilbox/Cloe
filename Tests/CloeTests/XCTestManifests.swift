a3175e62fcea53a737569b1bfda80e20d02e486b
Gil Birman <gil.birman@airbnb.com>
2020-01-23 21:39:14 -0800

    update XCTestManifests

--- Tests/CloeTests/XCTestManifests.swift
+++ Tests/CloeTests/XCTestManifests.swift
@@ -3,8 +3,13 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
   return [
+    testCase(CloeTests.allTests),
     testCase(RetainedPublisherActionTests.allTests),
     testCase(PublisherActionTests.allTests),
+    testCase(PublisherStatusDispatcherTests.allTests),
+    testCase(PublisherStateDispatcherTests.allTests),
+    testCase(HandleCleanupTests.allTests),
+    testCase(CombinedReducersTests.allTests),
   ]
 }
 #endif
